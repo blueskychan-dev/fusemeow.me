767b62f162317cfc736bb457c8361d266a2baf6a
Phapoom Saksri <takemypic5121@gmail.com>
2023-12-20 23:17:45 +0700

    FINAL UPDATE OF THIS WEBSITE

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,13 +43,14 @@ var today = currentDate.toLocaleString(undefined, options);
 
 
  // why ur cheating?
-  var irlfriends24 = ["omsin", "cha (uhh)", "save (dangerous!!!)", "toto (good child)", "dis (too gay)" ]
-  var hate = [""]
+  var irlfriends24 = ["no one :3" ]
+  var hate = ["223.207.246.70"]
   var love = ["liem", "twisty", "and everyone", "is my best freinds :)"]
-  var oneword = ["เสก"]
+  var oneword = ["Chipi Chipi Chapa Chapa Dubi Dubi Daba Daba Magico Mi Dubi Dubi BOOM BOOM BOOM"]
   
   
-  const birthdate = new Date(2009, 12, 17); 
+
+  var birthdate = new Date(2009, 11, 17); // why 11 is 12? because month is zero-based, so we add 1
   console.log(`DEBUG: ${myage(birthdate)} (${today})`)
   
   
@@ -58,14 +59,15 @@ var today = currentDate.toLocaleString(undefined, options);
   
 document.getElementById("age").innerHTML = `${myage(birthdate)}`;
 console.log("Host in github pages ❤️ (https://pages.github.com/)")
-console.log("---Credit--- \nazuremist_ for development and apporve this code!\ntwistynado for recommends fonts! (also developemnt too :D)\nChatGPT/AI for help me to make this code!\n---Credit---")
+console.log("---Credit--- \bluestar_ for development and apporve this code!\ntwistynado for recommends fonts! (also developemnt too :D)\nChatGPT/AI for help me to make this code!\n---Credit---")
 console.log("I wanna be a girl :3")
-console.log("Trans child (hidden) vs transphobia/homophobic family")
+console.log("Trans child (hidden) vs transphobia/homophobic family and bully")
 console.log("Trying live with this shit world...")
-console.log("Did you know?\nM.2/4 is gay so much!")
-console.log("EASTEREGG: we have a few arrays in this code, try to find them all!")
-console.log("if you found it!, try to run this code in your console: ")
-console.log("array.forEach(tell => {\n    console.log(tell);\n});");
-console.log("replace array with the array you found, and see what happens!")
-console.log("good luck to found all! (easy)")
-  
+console.log("Did you know?\nM.2/4 is fucking the hell...")
+// print big red warning message "!! WARNING !!"
+console.log('%c !! HOLD UP !!', 'color: red; font-size: 50px;');
+console.log('%c This website/script and domain will get the final update in 20 December 2023, after that will no any updates in here, we are moving to https://blueskychan.dev soon! Thanks you for following me :3', 'color: black; background-color: yellow; font-size: 16px;');
+console.log('Whatever, console.log(oneword)')
+setTimeout(function() {
+  alert('I have some news in console tabs, if you are in desktop, Please press F12 now ;-;');
+}, 3000);
